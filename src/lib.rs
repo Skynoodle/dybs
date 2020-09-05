4d933b7d852d5356bdf488a8900775cabbb195ea
Skynoodle <iainrobert.maciver@gmail.com>
2020-09-05 10:28:26 +0100

    Add type allowance for unsized data

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,16 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-pub struct Inner<T> {
+pub struct Inner<T: ?Sized> {
     count: AtomicUsize,
     data: T,
 }
 
-pub struct My<T> {
+pub struct My<T: ?Sized> {
     inner: *const Inner<T>,
 }
 
-pub struct Dyb<'l, T> {
+pub struct Dyb<'l, T: ?Sized> {
     inner: &'l Inner<T>,
 }
 
@@ -26,6 +26,8 @@ impl<T> My<T> {
             inner: Box::into_raw(Box::new(inner)),
         }
     }
+}
+impl<T: ?Sized> My<T> {
     pub fn borrow<'l>(&self) -> Dyb<'l, T> {
         // Safety:
         // This deref + borrow yields a reference with lifetime 'l.
@@ -42,7 +44,7 @@ impl<T> My<T> {
     }
 }
 
-impl<T> Deref for Dyb<'_, T> {
+impl<T: ?Sized> Deref for Dyb<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -50,7 +52,7 @@ impl<T> Deref for Dyb<'_, T> {
     }
 }
 
-impl<T> Drop for My<T> {
+impl<T: ?Sized> Drop for My<T> {
     fn drop(&mut self) {
         if unsafe { &*self.inner }.count.load(Ordering::Acquire) == 0 {
             drop(unsafe { Box::from_raw(self.inner as *mut Inner<T>) });
@@ -60,7 +62,7 @@ impl<T> Drop for My<T> {
     }
 }
 
-impl<T> Drop for Dyb<'_, T> {
+impl<T: ?Sized> Drop for Dyb<'_, T> {
     fn drop(&mut self) {
         self.inner.count.fetch_sub(1, Ordering::Release);
     }
@@ -82,7 +84,9 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "My pointer dropped with outstanding Dybs - this will leak the resource")]
+    #[should_panic(
+        expected = "My pointer dropped with outstanding Dybs - this will leak the resource"
+    )]
     fn invalid_drop_order_panics() {
         let my = My::new(());
         let _dyb = my.borrow();
